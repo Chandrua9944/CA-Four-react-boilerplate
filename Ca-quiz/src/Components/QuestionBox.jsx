34a378bc669dc59df804e32d05c291df2e243bc3
unknown <chandrua9944@gmail.com>
2024-06-22 12:59:36 +0530

    Succes

--- Ca-quiz/src/Components/QuestionBox.jsx
+++ Ca-quiz/src/Components/QuestionBox.jsx
@@ -1,46 +1,58 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react'
-import questions from '../../../src/questions'
-import Result from './Result'
+import { useState } from 'react';
+import questions from '../../../src/questions';
+import Result from './Result';
 
 export default function QuestionBox(props) {
-  let color=props.props
-  let[highlight,setHighlight]=useState(false)
-  let css={
-    background:color?"#a020f0":"#ffffba",
-  }
-  
-  let highlightStyle={
-    color:highlight?"red":"black"
-  }
+  const color = props.props; 
+  const [highlight, setHighlight] = useState(false);
+  const [questionNo, setQuestionNo] = useState(0);
+  const [score, setScore] = useState(0);
 
-  let[questionNo,setquestionNo]=useState(0)
-  let[score,setScore]=useState(0)
+  const css = {
+    background: color ? "#a020f0" : "#ffffba",
+  };
 
-  let handleOption=(e)=>{
-    console.log(e.target.value)
-    setScore((e.target.value==='true'?score+1:score))
-    setquestionNo(questionNo+1)
-  }
+  const highlightStyle = {
+    color: highlight ? "red" : "black",
+  };
+
+  const handleOption = (e) => {
+    const isCorrect = e.target.value === 'true';
+    setScore(isCorrect ? score + 1 : score);
+    setQuestionNo(questionNo + 1);
+  };
 
   return (
     <>
-      {questionNo <5 ?(
+      {questionNo < 5 ? (
         <div className='question-box' style={css}>
-          <h2>Question: {questionNo+1}</h2>
-          <h3 style={highlightStyle}>{questions[questionNo]}</h3>
+          <h2>Question: {questionNo + 1}</h2>
+          <h3 style={highlightStyle}>{questions[questionNo].text}</h3>
           <div className='option-box'>
-            {questions[questionNo].options.map((e)=>(
-              <button className='options' value={e.isCorrect} key={e.id} onClick={handleOption}>{e}</button>
+            {questions[questionNo].options.map((option) => (
+              <button
+                className='options'
+                value={option.isCorrect}
+                key={option.id}
+                onClick={handleOption}
+              >
+                {option.text}
+              </button>
             ))}
           </div>
           <div>
-            <button className='highlight-button' onClick={()=>setHighlight(true)}>Highlight</button>
-            <button className='highlight-button' onClick={()=>setHighlight(false)}>Remove Highlight</button>
+            <button className='highlight-button' onClick={() => setHighlight(true)}>
+              Highlight
+            </button>
+            <button className='highlight-button' onClick={() => setHighlight(false)}>
+              Remove Highlight
+            </button>
           </div>
         </div>
-      ):(<Result props={[score,color]}/>)
-    }
+      ) : (
+        <Result props={[score, color]} />
+      )}
     </>
-  )
+  );
 }
